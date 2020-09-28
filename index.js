f4782c84e3c676a7db4cb86165e8a157e71cfff1
ak44479 <alexanderkauffman23@gmail.com>
2020-09-27 20:14:29 -0400

    Updated the Header and the cards
    I added a div class to the js html string because it needed a name to help style it. I also got the css working for the cards but we need to talk in person to work on the data to loop it correctly. Right now the html loop of the js is not best apporach. Its working for tuesday talk.

--- index.js
+++ index.js
@@ -3,42 +3,45 @@ window.onload = () =>  {
 
     let cardTags = "";
 
-    for (let i = 0; i < 10; i++) {
-        cardTags += `<div> <div class="demo-card-square mdl-card mdl-shadow--2dp">
-        <div class="mdl-card__title mdl-card--expand">
-            <h2 class="mdl-card__title-text">Update</h2>
+    for (let i = 0; i < 24; i++) {
+        cardTags += `<div class="thecards"> 
+        
+        <div class="demo-card-square mdl-card mdl-shadow--2dp">
+            <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">Update</h2>
+            </div>
+            <div class="mdl-card__supporting-text">
+              <div class="text-space">
+                   <b>Name:</b> Joe Smith
+               </div>
+               <div class="text-space">
+                   <b>Status:</b> 3rd year New Media Design
+               </div>
+                <div class="text-space">
+                   <b>Email:</b> abcd@rit.edu
+               </div>
         </div>
-        <div class="mdl-card__supporting-text">
-           <div class="text-space">
-                <b>Name:</b> Joe Smith
-            </div>
-            <div class="text-space">
-                <b>Status:</b> 3rd year New Media Design
-            </div>
-            <div class="text-space">
-                <b>Email:</b> abcd@rit.edu
-            </div>
 
-        </div>
-        <div class="mdl-card__actions">
-            <button class="mdl-button mdl-js-button mdl-button--raised view-pf-btn">
-                View Portfolio
-            </button>
-            <div class="interest-tags">
-                <button class="mdl-chip">
-                    <span class="mdl-chip__text"> #UI design</span>
+            <div class="mdl-card__actions">
+                <button class="mdl-button mdl-js-button mdl-button--raised view-pf-btn">
+                    View Portfolio
                 </button>
-                <button class="mdl-chip">
-                    <span class="mdl-chip__text">#3D modeling</span>
-                </button>
-                <button class="mdl-chip">
-                    <span class="mdl-chip__text">#UX design</span>
-                </button>
-                <button class="mdl-chip">
-                    <span class="mdl-chip__text">#Typography</span>
-                </button>
-            </div>
-        </div> </div>`;
+                <div class="interest-tags">
+                    <button class="mdl-chip">
+                        <span class="mdl-chip__text"> #UI design</span>
+                    </button>
+                    <button class="mdl-chip">
+                        <span class="mdl-chip__text">#3D modeling</span>
+                    </button>
+                    <button class="mdl-chip">
+                        <span class="mdl-chip__text">#UX design</span>
+                    </button>
+                    <button class="mdl-chip">
+                        <span class="mdl-chip__text">#Typography</span>
+                    </button>
+                </div>
+            </div> 
+        </div>`;
     }
     content.innerHTML = cardTags;
 }
